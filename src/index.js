a45ca1e050bfce4c6a5387d4901409f846da0ea1
medihebfaiza <fmohamediheb@gmail.com>
2018-10-21 16:14:35 +0200

    [Add] react-redux to entry point

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import rootReducer from './reducers';
 import App from './components/App'
 import { BrowserRouter } from 'react-router-dom'
 import './main.scss'
 
-ReactDOM.render(<BrowserRouter><App/></BrowserRouter>, document.getElementById('app'));
+const store = createStore(rootReducer)
+
+ReactDOM.render(<BrowserRouter><Provider store={store}><App/></Provider></BrowserRouter>, document.getElementById('app'));
