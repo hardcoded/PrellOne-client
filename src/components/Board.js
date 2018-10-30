e57cfb300e165753a20b2fc5a07ade7ba675592b
Marion <marion.mary@hotmail.fr>
2018-10-30 16:35:51 +0100

    [update] board design

--- src/components/Board.js
+++ src/components/Board.js
@@ -2,7 +2,7 @@ import React from 'react'
 import ListPrello from '../containers/ListPrello.container'
 import ListPrelloCompo from '../components/ListPrello'
 import AddList from '../containers/AddList.container'
-import { Container } from 'reactstrap'
+import { Container, Row, Col } from 'reactstrap'
 import PropTypes from  'prop-types'
 
 const Board = ({id, title, lists}) => (
@@ -15,15 +15,22 @@ const Board = ({id, title, lists}) => (
       </header>
       <section>
         <Container style={{maxWidth:"100%"}}>
+        <Row className="scrolling-wrapper-flexbox">
         {
           lists.map(list=> (
+          <Col className="list" xs="12" sm="12" md="6" lg="3">
           <ListPrello key={list} listId={list}></ListPrello>
+          </Col>
         ))}
+        <Col>
         <AddList boardId={id}></AddList>
+        </Col>
+        </Row>
         </Container>
       </section>
     </div>
 )
+
 
 Board.propTypes={
   id: PropTypes.string.isRequired,
