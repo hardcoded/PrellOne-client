706eac92be5785815a67e6782a637870a0b2d45a
Marion <marion.mary@hotmail.fr>
2018-10-30 17:33:12 +0100

    [update] some things

--- src/components/ModalPrello.js
+++ src/components/ModalPrello.js
@@ -2,25 +2,24 @@ import React from 'react';
 import { Modal, ModalHeader, ModalBody, ModalFooter, Button} from 'reactstrap';
 import PropTypes from  'prop-types'
 
-const ModalPrello = ({modal, title, description, closeModal, openModal})=>(
+const ModalPrello = ({modal, title, description, closeModal})=>(
   <div>
     <Modal isOpen={modal} toggle={closeModal} >
         <ModalHeader toggle={closeModal}>{title}</ModalHeader>
         <ModalBody>
-          <h5>{description}</h5>
-          <p>Micro</p>
+          <h5>Description</h5>
+          <p>{description}</p>
           <h5>Members</h5>
         </ModalBody>
         <ModalFooter>
-          <Button color="secondary" onClick={closeModal}>Cancel</Button>
+          <Button color="success" onClick={closeModal}>Cancel</Button>
         </ModalFooter>
    </Modal>
 </div>
   )
 
 ModalPrello.propTypes={
-  closeModal: PropTypes.func.isRequired,
-  openModal: PropTypes.func.isRequired
+  closeModal: PropTypes.func.isRequired
   
 }
 export default ModalPrello
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,12 +1,10 @@
 import { combineReducers } from 'redux'
-import { reducer as formReducer } from 'redux-form'; 
 import reducerCardPrello from './cardPrello.reducer'
 import reducerListPrello from './listPrello.reducer'
 import reducerBoard from './board.reducer'
 import reducerModal from './modal.reducer'
 
 export default combineReducers({
-    form: formReducer,
     reducerCardPrello,
     reducerBoard,
     reducerListPrello,
