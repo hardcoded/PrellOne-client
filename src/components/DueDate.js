eeebaed0470d0972ab1f26d2e01a750483afda17
medihebfaiza <fmohamediheb@gmail.com>
2018-11-02 11:11:37 +0100

    [Add] Toggle done card

--- src/components/DueDate.js
+++ src/components/DueDate.js
@@ -3,24 +3,27 @@ import { Badge } from 'reactstrap';
 import PropTypes from  'prop-types';
 import moment from 'moment';
 
-const DueDate = ({date})=>{
+const DueDate = ({date, done})=>{
   const now = moment()
   console.log(now.format());
   
   const dueDate = moment(date)
   const diff = dueDate.diff(now, 'days')
   var color = 'success'
-  if (diff <= 3) {
-    color = 'warning'
-  } 
-  if (diff <= 1) {
-    color = 'danger'
+  if (!done){
+    if (diff <= 3) {
+      color = 'warning'
+    } 
+    if (diff <= 1) {
+      color = 'danger'
+    }
   }
   return(
   <Badge color={color}>{dueDate.format("dddd, MMMM Do YYYY, h:mm a")}</Badge>
   )}
 
 DueDate.propTypes={
-  date: PropTypes.string.isRequired
+  date: PropTypes.string.isRequired,
+  done: PropTypes.bool.isRequired
 }
 export default DueDate
