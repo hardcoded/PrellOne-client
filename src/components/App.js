d408d388e18195e1d195bae3f6ee10cee0dcec44
Unknown <alex.bottero@hotmail.fr>
2018-11-06 16:13:34 +0100

    home with back

--- src/components/App.js
+++ src/components/App.js
@@ -7,11 +7,11 @@ import SignIn from '../containers/SignIn.container'
 import { Switch, Route } from 'react-router-dom'
 import TeamsManager from '../containers/TeamsManager.container';
 
-const App = () => (
+const App = ({dispatch}) => (
     <div>
       <AppNav />
       <Switch>
-      <Route exact path='/' component={Home}/>
+      <Route exact path='/' component={Home} dispatch={dispatch}/>
       <Route path='/board/:boardId' component={Board}/>
       <Route path='/team' component={TeamsManager}/>
       <Route path='/signUp' component={SignUp}/>
--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore,applyMiddleware } from 'redux';
 import rootReducer from './reducers';
+import thunk from 'redux-thunk'
 import App from './components/App'
 import { BrowserRouter } from 'react-router-dom'
 import './main.scss'
 
 const store = createStore(
   rootReducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ &&
-  window.__REDUX_DEVTOOLS_EXTENSION__()
+  applyMiddleware(thunk)
   )
 
 ReactDOM.render(<BrowserRouter><Provider store={store}><App/></Provider></BrowserRouter>, document.getElementById('app'));
