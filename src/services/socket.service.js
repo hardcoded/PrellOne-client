92482e02c1930968a3f898d8721f2e0571286977
Unknown <alex.bottero@hotmail.fr>
2018-11-09 15:10:56 +0100

    start socket

---
+++ src/services/socket.service.js
@@ -0,0 +1,21 @@
+import io from 'socket.io-client'
+
+const socket=io('http://localhost:8080')
+
+socket.on("error",(error)=>{
+    console.log(error)
+})
+
+export default {
+    init(store){
+        socket.on( "action" ,(action)=>
+            store.dispatch(action)
+        )
+        socket.on("connect",()=>
+            console.log("user connected!")
+        )
+    },
+    emit(type,payload){
+        socket.emit(type,payload)
+    }
+}
