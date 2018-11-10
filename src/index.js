0b310a91c553a2670c506bd38edcfa6b4a63f0d5
Johan Brunet <brunet.johan.23@gmail.com>
2018-11-10 18:37:59 +0100

    [fix] Set user data in store at all times

--- src/index.js
+++ src/index.js
@@ -2,18 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware, compose } from 'redux';
-import rootReducer from './reducers';
+import reducers from './reducers';
 import thunk from 'redux-thunk'
 import App from './components/App'
 import { BrowserRouter } from 'react-router-dom'
 import './main.scss'
 
 const store = createStore(
-  rootReducer,
+  reducers,
   window.__REDUX_DEVTOOLS_EXTENSION__ ? compose(
     applyMiddleware(thunk),
     window.__REDUX_DEVTOOLS_EXTENSION__()
   ) : applyMiddleware(thunk)
 )
+
+export default store
 
 ReactDOM.render(<BrowserRouter><Provider store={store}><App/></Provider></BrowserRouter>, document.getElementById('app'));
