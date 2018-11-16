6490f189000f63b5f013865f39c7b96eb7367b6b
Marion Rul <marion.mary@hotmail.fr>
2018-11-16 13:12:18 +0100

    [add] team to board with board to team

--- src/reducers/team.reducer.js
+++ src/reducers/team.reducer.js
@@ -7,6 +7,12 @@ const team = (state = {}, action) => {
                 return map;
             }, {});
 
+        case 'BOARD_FETCHED':
+            return action.payload.teams.reduce((map, team) => {
+                map[team.id] = team;
+                return map;
+            }, {});
+
         case 'TEAM_ADDED': 
             return {
                 ...state,
@@ -87,7 +93,12 @@ const team = (state = {}, action) => {
             else {
                 return state
             }
-            
+        case 'TEAM_ADDED_BOARD':
+            return {
+                ...state,
+                [action.team.id]: action.team
+            } 
+           
         default:
             return state
     }
