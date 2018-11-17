b49866121d015792404b845dd28f7309d1e68be1
Unknown <alex.bottero@hotmail.fr>
2018-11-17 01:44:07 +0100

    FIX

--- src/containers/Team.container.js
+++ src/containers/Team.container.js
@@ -1,16 +1,34 @@
 import { connect } from 'react-redux'
 import Team from '../components/Team'
 import { openModalCreateBoard } from '../actions/addBoard.action'
+import React, { Component } from 'react'
+import Spinner from '../components/Spinner'
+class TeamContainer extends Component {
 
+  render() {
+    if (this.props.id) {
+      return (<Team {...this.props}></Team>)
+    }
+    else {
+      return (<Spinner></Spinner>)
+    }
+  }
+}
 
 const mapStateToProps = (state, ownProps) => {
   const teamId = ownProps.teamId 
-  return({
-    id: state.reducerTeam[teamId].id,
-    name: state.reducerTeam[teamId].name,
-    boards: state.reducerTeam[teamId].boards,
-    members: state.reducerTeam[teamId].members
-  })}
+  if(state.reducerTeam[teamId]){
+    return({
+      id: state.reducerTeam[teamId].id,
+      name: state.reducerTeam[teamId].name,
+      boards: state.reducerTeam[teamId].boards,
+      members: state.reducerTeam[teamId].members
+    })
+  }
+  else{
+    return undefined
+  }
+}
 
 const mapDispatchToProps = dispatch => ({
   openModalCreateBoard: (teamId) => {
@@ -20,4 +38,4 @@ const mapDispatchToProps = dispatch => ({
 export default connect(
   mapStateToProps,
   mapDispatchToProps
-)(Team)
+)(TeamContainer)
